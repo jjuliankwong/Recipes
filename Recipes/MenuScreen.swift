fbbd64d0389a9a32a50f43babb3c8e7a19826620
jjuliankwong <jacob.juliankwong@menloschool.org>
2016-05-18 09:06:01 -0700

    initScreenEdits

--- Recipes/MenuScreen.swift
+++ Recipes/MenuScreen.swift
@@ -12,12 +12,31 @@ class MenuScreen: UIViewController
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let button = UIButton(type: UIButtonType.System) as UIButton
-        button.frame = CGRect(x: 100, y: 100, width: 50, height: 50)
-        button.backgroundColor = UIColor.orangeColor()
-        button.setTitle("See My Recipes", forState: UIControlState.Normal)
-        button.addTarget(self, action: "goToRecipeScreen", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(button)
+        
+        // Set background
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        // Set up logo
+        let logo = UIImage(named: "logo")
+        let logoView = UIImageView(image: logo)
+        logoView.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        view.addSubview(logoView)
+        
+        // Set up "See My Recipes" Button
+        let seeMyRecipesButton = UIButton(type: UIButtonType.System) as UIButton
+        seeMyRecipesButton.frame = CGRect(x: 100, y: 100, width: 50, height: 50)
+        seeMyRecipesButton.backgroundColor = UIColor.orangeColor()
+        seeMyRecipesButton.setTitle("See My Recipes", forState: UIControlState.Normal)
+        seeMyRecipesButton.addTarget(self, action: "goToRecipeScreen", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(seeMyRecipesButton)
+        
+        // Set up "add Recipe" Button
+        let addRecipeButton = UIButton(type: UIButtonType.System) as UIButton
+        addRecipeButton.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+        addRecipeButton.backgroundColor = UIColor.orangeColor()
+        addRecipeButton.setTitle("Add Recipe", forState: UIControlState.Normal)
+        addRecipeButton.addTarget(self, action: "AddRecipeScreen", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(addRecipeButton)
         
     }
     
