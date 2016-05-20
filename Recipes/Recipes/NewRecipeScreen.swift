070c9ff489fc996a88d2992fa4f7e0183cc44c4e
jessicahoughton <scotthoughton@Scotts-MacBook-Air-2.local>
2016-05-20 14:34:11 -0700

    add more options for recipes

--- Recipes/Recipes/NewRecipeScreen.swift
+++ Recipes/Recipes/NewRecipeScreen.swift
@@ -24,9 +24,8 @@ class NewRecipeScreen: UIViewController
         
         // Prompt
         let newRecipeLabel: UILabel = UILabel()
-        newRecipeLabel.frame = CGRectMake(50, 50, 300, 15)
-        newRecipeLabel.centerXAnchor
-        newRecipeLabel.font = UIFont(name: "Georgia", size: 12)
+        newRecipeLabel.frame = CGRectMake(40, 60, 300, 20)
+        newRecipeLabel.font = UIFont(name: "Georgia", size: 15)
         newRecipeLabel.backgroundColor = UIColor.whiteColor()
         newRecipeLabel.textColor = UIColor.blackColor()
         newRecipeLabel.textAlignment = NSTextAlignment.Center
@@ -35,17 +34,17 @@ class NewRecipeScreen: UIViewController
         
         // Set up Appetizer Button
         let appetizerButton = UIButton()
-        appetizerButton.frame = CGRect(x: 50, y: 100, width: 150, height: 40)
+        appetizerButton.frame = CGRect(x: 133, y: 100, width: 110, height: 40)
         appetizerButton.layer.cornerRadius = 10
         appetizerButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
         appetizerButton.backgroundColor = UIColor.blueColor()
         appetizerButton.setTitle("Appetizer Recipe", forState: UIControlState.Normal)
-        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        appetizerButton.addTarget(self, action: "makeAppetizerRecipe:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(appetizerButton)
         
         // Set up Bread Button
         let breadButton = UIButton()
-        breadButton.frame = CGRect(x: 50, y: 150, width: 130, height: 40)
+        breadButton.frame = CGRect(x: 133, y: 150, width: 110, height: 40)
         breadButton.layer.cornerRadius = 10
         breadButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
         breadButton.backgroundColor = UIColor.blueColor()
@@ -53,14 +52,96 @@ class NewRecipeScreen: UIViewController
         //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(breadButton)
         
+        // Set up Breakfast Button
+        let breakfastButton = UIButton()
+        breakfastButton.frame = CGRect(x: 133, y: 200, width: 110, height: 40)
+        breakfastButton.layer.cornerRadius = 10
+        breakfastButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        breakfastButton.backgroundColor = UIColor.blueColor()
+        breakfastButton.setTitle("Breakfast Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(breakfastButton)
         
-
+        // Set up Salad Button
+        let saladButton = UIButton()
+        saladButton.frame = CGRect(x: 133, y: 250, width: 110, height: 40)
+        saladButton.layer.cornerRadius = 10
+        saladButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        saladButton.backgroundColor = UIColor.blueColor()
+        saladButton.setTitle("Salad Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(saladButton)
+        
+        // Set up Sandwhich Button
+        let sandwhichButton = UIButton()
+        sandwhichButton.frame = CGRect(x: 133, y: 300, width: 110, height: 40)
+        sandwhichButton.layer.cornerRadius = 10
+        sandwhichButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        sandwhichButton.backgroundColor = UIColor.blueColor()
+        sandwhichButton.setTitle("Sandwhich Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(sandwhichButton)
+        
+        // Set up Soup Button
+        let soupButton = UIButton()
+        soupButton.frame = CGRect(x: 133, y: 350, width: 110, height: 40)
+        soupButton.layer.cornerRadius = 10
+        soupButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        soupButton.backgroundColor = UIColor.blueColor()
+        soupButton.setTitle("Bread Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(soupButton)
+        
+        // Set up Entree Button
+        let entreeButton = UIButton()
+        entreeButton.frame = CGRect(x: 133, y: 400, width: 110, height: 40)
+        entreeButton.layer.cornerRadius = 10
+        entreeButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        entreeButton.backgroundColor = UIColor.blueColor()
+        entreeButton.setTitle("Entree Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(entreeButton)
+        
+        // Set up Dessert Button
+        let dessertButton = UIButton()
+        dessertButton.frame = CGRect(x: 133, y: 450, width: 110, height: 40)
+        dessertButton.layer.cornerRadius = 10
+        dessertButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        dessertButton.backgroundColor = UIColor.blueColor()
+        dessertButton.setTitle("Dessert Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(dessertButton)
+        
+        // Set up Drink Button
+        let drinkButton = UIButton()
+        drinkButton.frame = CGRect(x: 133, y: 500, width: 110, height: 40)
+        drinkButton.layer.cornerRadius = 10
+        drinkButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        drinkButton.backgroundColor = UIColor.blueColor()
+        drinkButton.setTitle("Drink Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(drinkButton)
+        
+        // Set up Sauce Button
+        let sauceButton = UIButton()
+        sauceButton.frame = CGRect(x: 133, y: 550, width: 110, height: 40)
+        sauceButton.layer.cornerRadius = 10
+        sauceButton.titleLabel!.font =  UIFont(name: "Georgia", size: 12)
+        sauceButton.backgroundColor = UIColor.blueColor()
+        sauceButton.setTitle("Sauce Recipe", forState: UIControlState.Normal)
+        //appetizerButton.addTarget(self, action: "goToNewRecipeScreen:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(sauceButton)
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func makeAppetizerRecipe(sender: UIButton!)
+    {
+        
     }
     
 
