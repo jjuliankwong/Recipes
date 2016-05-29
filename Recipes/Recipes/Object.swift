3e1f0137150a33e2478dc949c79e76190c0ad266
jjuliankwong <jacob.juliankwong@menloschool.org>
2016-05-29 15:53:58 -0700

    what i going on

---
+++ Recipes/Recipes/Object.swift
@@ -0,0 +1,15 @@
+//
+//  Object.swift
+//  Recipes
+//
+//  Created by Jacob on 5/28/16.
+//  Copyright © 2016 JAJ. All rights reserved.
+//
+
+import Foundation
+import Realm
+
+public class Obj: RLMObject
+{
+    
+}
