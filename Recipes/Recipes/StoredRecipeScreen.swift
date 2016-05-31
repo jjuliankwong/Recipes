2b4ac9994dba4cf991152253b8dcdc1c95d88933
jessicahoughton <scotthoughton@Scotts-MacBook-Air-2.local>
2016-05-31 00:34:19 -0700

    fixes comments

--- Recipes/Recipes/StoredRecipeScreen.swift
+++ Recipes/Recipes/StoredRecipeScreen.swift
@@ -39,7 +39,7 @@ class StoredRecipeScreen: UIViewController
         menuButton.addTarget(self, action: "goToMenuScreen:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(menuButton)
         
-        // Set up Title
+        // Set up Recipe Name
         let name: UILabel = UILabel()
         name.frame = CGRectMake(0, 60, 375, 50)
         name.font = UIFont(name: "Georgia", size: 20)
@@ -49,7 +49,7 @@ class StoredRecipeScreen: UIViewController
         name.text = recipe!.recipeName
         self.view.addSubview(name)
         
-        // Set up Title
+        // Set up Ingredient Label
         let ingred: UILabel = UILabel()
         ingred.frame = CGRectMake(10, 100, 375, 20)
         ingred.font = UIFont(name: "Georgia", size: 15)
@@ -62,7 +62,7 @@ class StoredRecipeScreen: UIViewController
         
         var ingredCounter = 0
         for ingredient in recipe!.ingredients {
-            // Set up Title
+            // prints all ingredients
             let specIngred: UILabel = UILabel()
             specIngred.frame = CGRectMake(20, (CGFloat)(120 + (15*ingredCounter)), 375, 15)
             specIngred.font = UIFont(name: "Georgia", size: 10)
@@ -73,7 +73,7 @@ class StoredRecipeScreen: UIViewController
             ingredCounter++
         }
         
-        // Set up Title
+        // Set up Instruction label
         let instruct: UILabel = UILabel()
         instruct.frame = CGRectMake(10, (CGFloat)(140 + (15*ingredCounter)), 175, 20)
         instruct.font = UIFont(name: "Georgia", size: 15)
@@ -84,13 +84,13 @@ class StoredRecipeScreen: UIViewController
         
         var instructCounter = 0
         for instruction in recipe!.instructions {
-            // Set up Title
+            // prints all instructions
             let specInstruct: UILabel = UILabel()
             specInstruct.frame = CGRectMake(20, (CGFloat)(120 + (15*ingredCounter) + 40 + 15*instructCounter), 375, 15)
             specInstruct.font = UIFont(name: "Georgia", size: 10)
             specInstruct.backgroundColor = UIColor.whiteColor()
             specInstruct.textColor = UIColor.blackColor()
-            specInstruct.text = recipe!.instructions[instructCounter].instruct
+            specInstruct.text = (String)(instructCounter + 1) + ". " + recipe!.instructions[instructCounter].instruct
             self.view.addSubview(specInstruct)
             instructCounter++
         }
